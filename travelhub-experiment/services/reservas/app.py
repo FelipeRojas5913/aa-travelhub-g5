7f39f8129157bf30354575d56ad63e0dfcb287a7
camiloroa112 <camiloroa112@gmail.com>
2026-02-21 19:28:11 -0500

    Health endpoints microservicios.

---
+++ travelhub-experiment/services/reservas/app.py
@@ -0,0 +1,31 @@
+# 3rd Party Libraries
+import time
+from datetime import datetime, timezone
+from flask import Flask, jsonify, request
+
+# Instanciar app
+app = Flask(__name__)
+
+# Establecer el servicio intento del servicio
+service_state = {"status": "healthy", "delay": 0}
+
+# Api de Health
+@app.route('/health', methods = ['GET'])
+def health():
+    """Permite consultar el estado del servicio"""
+
+    # En caso de que se encuentre "unhealthy", se devuelve un código de estado 500
+    if service_state["status"] == "unhealthy":
+        return jsonify({"service": "reservas", "status": "unhealthy"}), 500
+    
+    # En caso de estar "degraded", agrega latencia artificial
+    elif service_state["status"] == "degraded":
+        time.sleep(service_state["delay"])
+    
+    # En caso de que se encuentre "healthy", se devuelve un código de estado 200
+    else:
+        return jsonify({"service": "reservas", "status": service_state["status"], "timestamp": datetime.now(timezone.utc)}), 200
+    
+# Iniciar servicio
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5003, debug = True)
